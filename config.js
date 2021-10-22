28099d926422a980d54cbb849367d74d7c3dab44
supuna <92195424+supuna28@users.noreply.github.com>
2021-10-22 08:02:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 userbot = {
- owner: [ "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+ owner: [ "94753943957","","94756883698", "62822980698995", "436504463151666", "62823283033323" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
-   wait: "tunggu sedang di proses...",
+   wait: "පොඩ්ඩක් ඉම්න 🌚⚡...",
    error: "errorr!!"
    },
-   prefix: ["/"],
+   prefix: ["."],
    gexp: 50,
    limit: 100,
-   packname: 'RixleBot',
-   author: '@_RizkyAdiNur',
+   packname: 'BOT KINGDOM Bot',
+   author: '@_botkingdom⚡',
     setting: {
     admin: "only admin",
     group: "only group",
     owner: "owner only pack",
     jadibot: "jadibot only",
-    botadmin: "bot harus menjadi admin"
+    botadmin: "bot  admin"
     }
 }
 module.exports = userbot
